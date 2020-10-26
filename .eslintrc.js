2973263691819bcf5b6d45b291aecc363343f78b
Hideki Igarashi <hideki.develop@gmail.com>
2020-10-26 20:25:50 +0900

    chore: Set up ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: ["@cybozu/eslint-config/presets/node", "@cybozu/eslint-config/presets/typescript"],
+  env: {
+    node: true,
+    es6: true
+  },
+  parserOptions: {
+    ecmaVersion: 9,
+    sourceType: "module",
+    project: "./tsconfig.eslint.json"
+  },
+  rules: {
+    "quotes": ["error", "double", { "avoidEscape": true }],
+    "object-curly-spacing": ["error", "always", { "arraysInObjects": true }],
+    "node/no-unsupported-features/es-syntax": "off"
+  }
+};
